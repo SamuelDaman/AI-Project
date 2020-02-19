928f18b1d06545cdc74344537e0a4e50adbf026e
Samuel Daman <SDamanProgramming@gmail.com>
2020-02-19 12:52:46 -0800

    Starting Dijkstras

---
+++ Assets/Scripts/Dijkstras.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dijkstras : MonoBehaviour
+{
+    List<GameObject> openList;
+    List<GameObject> closedList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+    public struct Node
+    {
+        public Vector3 location;
+        public GameObject north;
+        public GameObject south;
+        public GameObject east;
+        public GameObject west;
+    }
+
+    List<List<Node>> graph;
+    public Vector2Int graphSize = new Vector2Int(3, 3);
+
+    public GameObject tile;
+
+    private void Start()
+    {
+        graph = new List<List<Node>>(graphSize.y);
+    }
+}
