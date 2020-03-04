ed907a6ec61cba20c89f529d5709eb57b4d97d86
Samuel Daman <SDamanProgramming@gmail.com>
2020-03-04 12:44:59 -0800

    Finished state machines and navmeshes

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    public bool isClosed = true;
+    private bool isMoving = false;
+    Vector3 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != targetPosition)
+        {
+            isMoving = true;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.25f);
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    public void OpenClose()
+    {
+        if (isMoving == false)
+        {
+            if (isClosed == true)
+            {
+                targetPosition = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
+                isClosed = false;
+            }
+            else if (isClosed == false)
+            {
+                targetPosition = new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
+                isClosed = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NavMeshAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    private Camera cam;
+    private GateScript gateScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 500));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Gate"))
+            {
+                hit.collider.GetComponent<GateScript>().OpenClose();
+            }
+        }
+    }
+}
